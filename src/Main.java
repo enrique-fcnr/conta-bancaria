3b5981f2d7b6c4e6b75769e140be3185e77438da
enrique-fcnr <enriquefcnrgta@gmail.com>
2025-07-30 12:28:22 -0300

    first structure

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Account;
+
+public class Main{
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Account conta1;
+
+        System.out.print("Enter account number: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter account holder: ");
+        String holder = sc.nextLine();
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
